4e7668cf1b29dff51048688078074775d9184588
Aisha-Yusuff <a.yusuff99@outlook.com>
2022-07-29 18:01:47 +0100

    Add title props to Header component and comment for CSS style variable

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,23 @@
-export const Header = () => {
+import PropTypes from "prop-types";
+
+export const Header = (props) => {
   return (
     <header>
-      <h1>Task Tracker</h1>
+      <h1>{props.title}</h1>
     </header>
   );
 };
+
+Header.defaultProps = {
+  title: "Task Tracker",
+};
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+// CSS in JS example
+// const headingStyle = {
+//   color: "blue",
+//   backgroundColor: "yellow",
+// };
