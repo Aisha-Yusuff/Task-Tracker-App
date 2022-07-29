374fc9b5d0832f2e9a1e93e75b6a31d04b6fbf2d
Aisha-Yusuff <a.yusuff99@outlook.com>
2022-07-29 19:07:39 +0100

    Add tasks component
    Create list for each task using an array and added useState for tasks to app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import { useState } from "react";
 import { Header } from "./components/Header";
+import { Tasks } from "./components/Tasks";
+
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Aug 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Dentist Appointment",
+      day: "Aug 7th at 1:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Food Shopping",
+      day: "Aug 8th at 5:30pm",
+      reminder: false,
+    },
+  ]);
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
